3b255f7d575fe8a011c23df3c0561adb4387cf0d
Walter Junior <walter.asjunior31@gmail.com>
2023-03-15 22:21:38 -0300

    inicio do projeto

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="col-md-6">
+            <div class="header-title">
+                <h2>Cadastrar Novo Usuário</h2>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <form action="" method="post">
+            <div class="d-flex">
+                <div class="p-2 flex-lg-fill">
+                    <input type="text" name="cpf" class="form-control" placeholder="CPF"><br>
+                </div>
+                <div class="p-2 flex-lg-fill">
+                    <input type="text" name="cpf" class="form-control" placeholder="CPF"><br>
+                </div>
+            </div>
+        </form>
+        <div class="row">
+            <div class="col-md-6 mt-2">
+                <form action="" method="post">
+                    
+                </form>
+            </div>
+        </div>
+    </div>
+    <select class="form-select">
+        <option selected>Estado</option>
+        <option value="ac">AC</option>
+        <option value="al">AL</option>
+        <option value="ap">AP</option>
+        <option value="am">AM</option>
+        <option value="ba">BA</option>
+        <option value="ce">CE</option>
+        <option value="df">DF</option>
+        <option value="es">ES</option>
+        <option value="go">GO</option>
+        <option value="ma">MA</option>
+        <option value="mt">MT</option>
+        <option value="ms">MS</option>
+        <option value="mg">MG</option>
+        <option value="pa">PA</option>
+        <option value="pb">PB</option>
+        <option value="pr">PR</option>
+        <option value="pe">PE</option>
+        <option value="pi">PI</option>
+        <option value="rj">RJ</option>
+        <option value="rn">RN</option>
+        <option value="rs">RS</option>
+        <option value="ro">RO</option>
+        <option value="rr">RR</option>
+        <option value="sc">SC</option>
+        <option value="sp">SP</option>
+        <option value="se">SE</option>
+        <option value="to">TO</option>
+    </select>
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+
+</html>
